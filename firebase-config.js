4188316ec78d239f2f1af7186eebca05627b5289
jchef50-commits <jchef50@gmail.com>
2026-06-08 21:03:52 -0600

    Agregar plantilla de configuración de Firebase

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+// Pega aquí la configuración web de Firebase cuando la tengas.
+// Ejemplo:
+// export const firebaseConfig = {
+//   apiKey: "TU_API_KEY",
+//   authDomain: "tu-proyecto.firebaseapp.com",
+//   projectId: "tu-proyecto",
+//   storageBucket: "tu-proyecto.firebasestorage.app",
+//   messagingSenderId: "123456789",
+//   appId: "1:123456789:web:abcdef123456"
+// };
+
+export const firebaseConfig = {
+  apiKey: "REEMPLAZAR_API_KEY",
+  authDomain: "REEMPLAZAR_AUTH_DOMAIN",
+  projectId: "REEMPLAZAR_PROJECT_ID",
+  storageBucket: "REEMPLAZAR_STORAGE_BUCKET",
+  messagingSenderId: "REEMPLAZAR_MESSAGING_SENDER_ID",
+  appId: "REEMPLAZAR_APP_ID"
+};
